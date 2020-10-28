1fe19337f13911c39e011dd373d1ca59cd042cda
vincentmuya <vincentmuya13@gmail.com>
2020-10-28 18:27:52 +0300

    called news items from the database to the news template

--- project/app/models.py
+++ project/app/models.py
@@ -7,16 +7,9 @@ class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True, null=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, null=True)
 
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse('article_list_by_category', args=[self.slug])
 
 class Article(models.Model):
     category = models.ForeignKey(Category, related_name='products', null=True, on_delete=models.CASCADE)
@@ -25,21 +18,6 @@ class Article(models.Model):
     image = models.ImageField(upload_to="posts/", blank=True, null=True)
     description = models.TextField(blank=True)
 
-    class Meta:
-        ordering = ('title',)
-        index_together = (('id', 'slug'),)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('article_detail', args=[self.id, self.slug])
-
-    @classmethod
-    def search_by_name(cls, search_term):
-        search_result = cls.objects.filter(name__icontains=search_term)
-        return search_result
+        return self.title
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from .models import Article, Category
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'categories': categories, 'articles': articles})
