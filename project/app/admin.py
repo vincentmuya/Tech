239dbab12a6f6324eec4c55223c331a3871375aa
vincentmuya <vincentmuya13@gmail.com>
2020-10-29 18:23:20 +0300

    created class accessories_category and class accessories and made migrations

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Article, Category
+from .models import Article, Category, AccessoriesCategory, Accessories
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Category)
+admin.site.register(AccessoriesCategory)
+admin.site.register(Accessories)
--- project/app/models.py
+++ project/app/models.py
@@ -27,6 +27,8 @@ class Article(models.Model):
     slug = models.SlugField(max_length=200, db_index=True, null=True)
     image = models.ImageField(upload_to="posts/", blank=True, null=True)
     description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('title',)
@@ -41,6 +43,51 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[self.id, self.slug])
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        search_result = cls.objects.filter(name__icontains=search_term)
+        return search_result
+
+
+class AccessoriesCategory(models.Model):
+    name = models.CharField(max_length=200, db_index=True, null=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, null=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'accesscategory'
+        verbose_name_plural = 'accessories_categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('article_list_by_category', args=[self.slug])
+
+
+class Accessories(models.Model):
+    accessoriescategory = models.ForeignKey(AccessoriesCategory, related_name='products', null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, null=True)
+    image = models.ImageField(upload_to="posts/", blank=True, null=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Accessories, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('accessories_detail', args=[self.id, self.slug])
 
     @classmethod
     def search_by_name(cls, search_term):
